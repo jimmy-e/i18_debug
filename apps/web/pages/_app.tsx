43f44d1146083aa082ca3c79f3579aaa008f87cd
Jimmy Emery <jimmy@jkbx.com>
2023-03-13 12:49:34 -0400

    updating app

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import Layout from 'layout';
 import { appWithTranslation } from 'next-i18next';
-import 'tailwind-config/tailwind.css';
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-);
+const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default appWithTranslation(App);
