5671a063a61c42db546086289f6f78e5c2a6c0a6
Jimmy Emery <jimmy@jkbx.com>
2023-03-13 12:57:22 -0400

    updating page

--- apps/web/next-i18next.config.js
+++ apps/web/next-i18next.config.js
@@ -10,8 +10,6 @@ module.exports = {
   keySeparator: '.',
   localeExtension: 'yml',
   localePath: '../../utils/copy',
-  // we are currently not handling different languages, when we wil, we can do something
-  // like `{{lng}}/{{ns}}`
   localeStructure: '{{ns}}',
   nsSeparator: '::',
 };
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-// todo: add typing
 export async function getStaticProps({ locale }) {
   return {
     props: {
