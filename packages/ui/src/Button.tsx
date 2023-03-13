fc7077e48acdf2fe5b421e5b7936a92fa52db976
Jimmy Emery <jimmy@jkbx.com>
2023-03-13 12:45:39 -0400

    removing packages

--- packages/ui/src/Button.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './Button.tailwind';
-
-export const Button: React.FC = () => (
-  <button className={styles.button} type="button">UI BUTTON</button>
-);
